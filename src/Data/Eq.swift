8cb6702e776d78aac6df178e7fd5e4c2fc8eee45
Paul Young <paulyoungonline@gmail.com>
2018-03-02 00:30:28 -0800

    Remove FFI binding for refIneq

--- src/Data/Eq.swift
+++ src/Data/Eq.swift
@@ -5,12 +5,6 @@ public extension Data.Eq._Foreign {
         }
     }
 
-    public static func refIneq<A: Equatable>(_ r1: A) -> (_ /* r2 */: A) -> Bool {
-        return { r2 in
-            return r1 != r2
-        }
-    }
-
     public static func eqArrayImpl<A: Equatable>(_ f: @escaping (A) -> (A) -> Bool) -> (_ /* xs */: [A]) -> (_ /* ys */: [A]) -> Bool {
         return { xs in
             return { ys in
